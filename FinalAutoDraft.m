b7e5870202d4050a46273913ed73fb6c8058dd55
jhoran4 <58038400+jhoran4@users.noreply.github.com>
2019-12-05 09:50:19 -0700

    Final Correct
    This program will be run twice. Once before keyboard controls and once after. The second one switches the first while loop from color == 3 to color == 3.

--- FinalAutoDraft.m
+++ FinalAutoDraft.m
@@ -1,59 +1,59 @@
 color = brick.ColorCode(4); 
+color = brick.ColorCode(4); 
 distance = brick.UltrasonicDist(3);
-angleRate = brick.GyroRate(2);
-brick.GyroCalibrate(2); 
- 
-
-    
-        
-    while color ~= 2 %%while color isn't blue
+    while color ~= 3 %%while color isn't green
         brick.GyroCalibrate(2);
         color = brick.ColorCode(4);
         distance = brick.UltrasonicDist(3);
-        brick.MoveMotor('B', 25);
-        brick.MoveMotor('C', 25.25);
+        brick.MoveMotor('B', 25.2);
+        brick.MoveMotor('C', 25);
         if color == 5 %%if it's red
             color = brick.ColorCode(4);
             brick.StopAllMotors();
             pause(3);
             brick.MoveMotor('B', 25);
-            brick.MoveMotor('C', 25.25);
+            brick.MoveMotor('C', 26);
             pause(3);
         end
-        
-        if distance <= 45.0000
+        while distance < 45
            distance = brick.UltrasonicDist(3);
-           brick.StopAllMotors();
-           brick.GyroCalibrate(2);
-           angleRate = brick.GyroRate(2);
-           pause(1);
-
-           while distance <= 45.0000
-              brick.GyroCalibrate(2);
-              distance = brick.UltrasonicDist(3);
-              angleRate = brick.GyroRate(2);
-          
-              brick.MoveMotor('B', -25);
-              brick.MoveMotor('C', 25);
-              pause(0.5);
-              if angleRate >= 1 && distance <= 30 
-                    brick.MoveMotor('B', 25);
-                    brick.MoveMotor('C', -25);
-                    pause(1.0);
-                    brick.GyroCalibrate(2);
-              end
-               
-           end
+           color = brick.ColorCode(4);
            
            brick.StopAllMotors();
-           brick.MoveMotor('B', 25);
-           brick.MoveMotor('C', 25.25);
            pause(1);
+           
+           brick.MoveMotorAngleRel('B', 25, 180);
+           brick.MoveMotorAngleRel('C', -25, 180);
+           pause(3);
+           
+           distance = brick.UltrasonicDist(3);
+           
+           if distance < 45 
+                brick.MoveMotorAngleRel('B', -25, 360);
+                brick.MoveMotorAngleRel('C', 25, 360);
+                pause (3);
+                distance = brick.UltrasonicDist(3);
+           end
+                
         end
         
-        brick.MoveMotor('B', 25);
-        brick.MoveMotor('C', 25.25);
-        
+           distance = brick.UltrasonicDist(3);
+           brick.StopAllMotors();
+           brick.MoveMotor('B', 25.2);
+           brick.MoveMotor('C', 25);
+           pause(1);
     end
+   
+
+    if color == 2
+        brick.StopAllMotors();
+        pause(5);
+        brick.MoveMotor('A', 25);
+        pause(5);
+        brick.MoveMotor('B', -25);
+        brick.MoveMotor('C', -25.5);
+        pause(5);
+    end
+    
     pause(4);
     brick.StopAllMotors();
